f3b1ca7cfe9d6997926b7da91a4de578ee7ff9c3
Liz635 <jhamell2680@gmail.com>
2026-02-07 11:12:18 -0400

    Fix: Configuracion Cloudinary

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static  # ← agrega esta línea
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+urlpatterns += staticfiles_urlpatterns()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
